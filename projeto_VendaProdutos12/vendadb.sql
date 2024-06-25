ee6ee84958488254d1e72d5e8ae54a6099bff5a1
victor <victorhs.almeida131@gmail.com>
2024-06-25 17:22:51 -0300

    att

--- projeto_VendaProdutos12/vendadb.sql
+++ projeto_VendaProdutos12/vendadb.sql
@@ -2,5 +2,10 @@ CREATE DATABASE vendadb;
 
 create table `vendadb`.`clientes`(
     `cliid` int not null auto_increment primary key,
-    `name` varchar(60) not null
-);
+    `name` varchar(60) not null,
+    `telefone` VARCHAR(20) NOT NULL,
+    `rua` VARCHAR(100) NOT NULL,
+    `genero` VARCHAR(30) NOT NULL,
+    `cidade` VARCHAR(50) NOT NULL,
+    `cpf` VARCHAR(20) NOT NULL
+); 
